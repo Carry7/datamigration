29d7d21caf616d1a4cc4d87a2788588f28f559d7
ASUS <1315175354@qq.com>
2020-04-10 14:28:16 +0800

    init

---
+++ src/test/java/cn/com/agree/datamigration/DatamigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.agree.datamigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatamigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
